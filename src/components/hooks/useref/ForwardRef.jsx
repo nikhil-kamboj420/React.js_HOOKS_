cbf018d0a20d96ebeca3273cc1de6df93a9e1cc0
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-03-08 23:06:13 +0530

    made some few changes

--- src/components/hooks/useref/ForwardRef.jsx
+++ src/components/hooks/useref/ForwardRef.jsx
@@ -1,25 +1,25 @@
 import { useRef } from "react";
 
-export const ForwardRef = () => {
+export const ForwardRef = ()=>{
     const username = useRef(null);
     const password = useRef(null);
-    const handleSubmit = (e) => {
+    const  handleSubmit= (e)=>{
         e.preventDefault();
-        console.log(username.current.value, password.current.value);
+        console.log(username.current.value,password.current.value);
     }
     return (
-        <form onSubmit={handleSubmit}>
-            <InputChild label="username" ref={username} />
-            <InputChild label="password" ref={password} type="password" />
+        <form className="h-[100vh] max-w-[100vw]" onSubmit={handleSubmit}>
+            <InputChild lable= {"username"} ref={username} type={"text"}/>
+            <InputChild lable= {"password"} ref={password} type={"password"}/>
             <button>submit</button>
         </form>
     )
 }
 
-const InputChild = ({ label, ref, type = "text" }) => {
+const InputChild = ({ lable, ref, type }) => {
     return (
         <div>
-            <label className="text-white">{label}</label>
+            <label className="text-white px-2.5">{lable}</label>
             <input className="text-2xl text-black w-3xs px-1.5 py-4 mb-4 bg-amber-300" type={type} ref={ref} />
         </div>
     )
